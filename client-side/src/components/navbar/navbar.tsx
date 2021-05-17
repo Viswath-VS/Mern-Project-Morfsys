2aa63381a57f93f9e6c7d7df62ff21f3ab8aa87b
Viswath-VS <viswathpersonnel@gmail.com>
2021-05-18 00:09:05 +0530

    css refactored

--- client-side/src/components/navbar/navbar.tsx
+++ client-side/src/components/navbar/navbar.tsx
@@ -9,7 +9,7 @@ import {navBarProps} from "../../types/proptype";
            <h1>Shenzhen Houde Academy</h1> 
            <div className="navbar-section">
                <h3>DISCOVER</h3>
-               <h3>IMPACT BOARD</h3>
+               <h3 className="section-active">IMPACT BOARD</h3>
                <h3>LEARNING PODS</h3>
            </div>
         </div>
